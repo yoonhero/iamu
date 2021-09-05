23adb385639d0d947a71962ee249a56aa08513d1
yoonhero <yoonhero06@naver.com>
2021-09-05 23:44:55 +0900

    #3.3 animated splash screen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { View } from "react-native";
 import LoggedInNav from "./navigators/LoggedInNav";
 import { StatusBar } from "expo-status-bar";
+import AnimatedSplash from "react-native-animated-splash-screen";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const onFinish = () => setLoading(true);
+
+  const preloadAssets = () => {
+    const fontToLoad = [Ionicons.font];
+    const fontPromises = fontToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [require("./assets/image/happy.png")];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    onFinish();
+  };
+  useEffect(() => {
+    preloadAssets();
+  }, []);
+
   return (
     <>
       <StatusBar style='inverted' />
-      <NavigationContainer>
-        {isLoggedIn ? <LoggedInNav /> : <View>login</View>}
-      </NavigationContainer>
+      <AnimatedSplash
+        translucent={true}
+        isLoaded={loading}
+        logoImage={require("./assets/image/happy.png")}
+        backgroundColor={"#fff"}
+        logoHeight={150}
+        logoWidth={150}>
+        <>
+          <NavigationContainer>
+            {isLoggedIn ? <LoggedInNav /> : <View>login</View>}
+          </NavigationContainer>
+        </>
+      </AnimatedSplash>
     </>
   );
 }
